ce21c73cbafc486cfb4884b60a0e866d31fa2daa
Deepak-Vohra <dvohra10@yahoo.com>
2024-05-27 18:36:23 -0400

    Update resource_server.go

--- resource_server.go
+++ resource_server.go
@@ -50,7 +50,7 @@ func resourceServerCreate(d *schema.ResourceData, m interface{}) error {
 }
 
 func resourceServerRead(d *schema.ResourceData, m interface{}) error {
-	environment_id := d.Get("environment_id").(string)
+	environment_id := d.Id()
 	client := resty.New()
 	resp, err := client.R().SetHeader("Accept", "application/json").SetAuthToken("eyJhbGciOiJFUzI1NiIsImtpZCI6ImU2ODAyNTc5NTVlOWFmYTUzZmRlY2U5YWNmYTMzMDYzZjEzYTYxZmI2OWRmMTg3YTI0NDdkYjFjOGQyMmRjMzAiLCJ0eXAiOiJKV1QifQ.eyJhY2Nlc3NfaWQiOiJMT01MTlZRNVZPN1JFU0xERUkyMzZEVUJZTFFTUlpJVFJPNDdCRjNVVTJXMzIiLCJhY3QiOnt9LCJjaWQiOiJwbGF0Zm9ybS1hcGktdXNlciIsImV4cCI6MTcxNjY5NzI4NiwiZ3JhbnQiOiJhcGlfdG9rZW4iLCJpYXQiOjE3MTY2OTYzODYsImlzcyI6Imh0dHBzOi8vYXV0aC5hcGkucGxhdGZvcm0uc2giLCJqdGkiOiIwMUhZU0g4MzA4OERLWlRLQ1g1UlpFWU1BSCIsIm5iZiI6MTcxNjY5NjM4NiwibnMiOiJwbGF0Zm9ybXNoIiwic3ViIjoiNzZjNjBlNDMtMDkzOC00NjI5LWFlMDAtY2IwYTU5YTQ0YzIwIn0.wXaw8BbQ3ufJoThpW-xKYy0a3P4ds8SdWAOCFBBb-LuZAR9GcUBa5HTcrWhkV13-j6jetnx8bLeYRzgBvrygFg").Get("https://ca-1.platform.sh/api/projects/zis3mqzwuqnu4/environments/" + environment_id)
 	if err != nil {
@@ -97,7 +97,7 @@ func resourceServerUpdate(d *schema.ResourceData, m interface{}) error {
 }
 
 func resourceServerDelete(d *schema.ResourceData, m interface{}) error {
-	environment_id := d.Get("environment_id").(string)
+	environment_id := d.Id()
 
 	d.SetId("")
 	client := resty.New()
