b61108cd468a20f27e5996cb55d1896e7153f0ec
Deepak-Vohra <dvohra10@yahoo.com>
2024-05-31 21:14:45 -0700

    Update resource_server.go

--- resource_server.go
+++ resource_server.go
@@ -34,7 +34,7 @@ func resourceServerCreate(d *schema.ResourceData, m interface{}) error {
 	environment_id := d.Get("environment_id").(string)
 
 	client := resty.New()
-	resp, err := client.R().SetHeader("Accept", "application/json").SetHeader("Content-Type", "application/json").SetBody("{'repository': 'git@github.com:Deepak-Vohra/greetings.git@master','profile': 'Greetings Project','files': [{'mode': 0600,'path': 'config.json','contents': '{}'}]}").SetAuthToken("eyJhbGciOiJFUzI1NiIsImtpZCI6ImU2ODAyNTc5NTVlOWFmYTUzZmRlY2U5YWNmYTMzMDYzZjEzYTYxZmI2OWRmMTg3YTI0NDdkYjFjOGQyMmRjMzAiLCJ0eXAiOiJKV1QifQ.eyJhY2Nlc3NfaWQiOiJMT01MTlZRNVZPN1JFU0xERUkyMzZEVUJZTFFTUlpJVFJPNDdCRjNVVTJXMzIiLCJhY3QiOnt9LCJjaWQiOiJwbGF0Zm9ybS1hcGktdXNlciIsImV4cCI6MTcxNjY5NzI4NiwiZ3JhbnQiOiJhcGlfdG9rZW4iLCJpYXQiOjE3MTY2OTYzODYsImlzcyI6Imh0dHBzOi8vYXV0aC5hcGkucGxhdGZvcm0uc2giLCJqdGkiOiIwMUhZU0g4MzA4OERLWlRLQ1g1UlpFWU1BSCIsIm5iZiI6MTcxNjY5NjM4NiwibnMiOiJwbGF0Zm9ybXNoIiwic3ViIjoiNzZjNjBlNDMtMDkzOC00NjI5LWFlMDAtY2IwYTU5YTQ0YzIwIn0.wXaw8BbQ3ufJoThpW-xKYy0a3P4ds8SdWAOCFBBb-LuZAR9GcUBa5HTcrWhkV13-j6jetnx8bLeYRzgBvrygFg").Post("https://ca-1.platform.sh/api/projects/zis3mqzwuqnu4/environments/" + environment_id + "/initialize")
+	resp, err := client.R().SetHeader("Accept", "application/json").SetHeader("Content-Type", "application/json").SetBody("{'repository': 'git@github.com:Deepak-Vohra/greetings.git@master','profile': 'Greetings Project','files': [{'mode': 0600,'path': 'config.json','contents': '{}'}]}").SetAuthToken("eyJhbGciOi").Post("https://ca-1.platform.sh/api/projects/zis3mqzwuqnu4/environments/" + environment_id + "/initialize")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -59,12 +59,12 @@ func resourceServerCreate(d *schema.ResourceData, m interface{}) error {
 func resourceServerRead(d *schema.ResourceData, m interface{}) error {
 	environment_id := d.Id()
 	client := resty.New()
-	resp, err := client.R().SetHeader("Accept", "application/json").SetAuthToken("eyJhbGciOiJFUzI1NiIsImtpZCI6ImU2ODAyNTc5NTVlOWFmYTUzZmRlY2U5YWNmYTMzMDYzZjEzYTYxZmI2OWRmMTg3YTI0NDdkYjFjOGQyMmRjMzAiLCJ0eXAiOiJKV1QifQ.eyJhY2Nlc3NfaWQiOiJMT01MTlZRNVZPN1JFU0xERUkyMzZEVUJZTFFTUlpJVFJPNDdCRjNVVTJXMzIiLCJhY3QiOnt9LCJjaWQiOiJwbGF0Zm9ybS1hcGktdXNlciIsImV4cCI6MTcxNjY5NzI4NiwiZ3JhbnQiOiJhcGlfdG9rZW4iLCJpYXQiOjE3MTY2OTYzODYsImlzcyI6Imh0dHBzOi8vYXV0aC5hcGkucGxhdGZvcm0uc2giLCJqdGkiOiIwMUhZU0g4MzA4OERLWlRLQ1g1UlpFWU1BSCIsIm5iZiI6MTcxNjY5NjM4NiwibnMiOiJwbGF0Zm9ybXNoIiwic3ViIjoiNzZjNjBlNDMtMDkzOC00NjI5LWFlMDAtY2IwYTU5YTQ0YzIwIn0.wXaw8BbQ3ufJoThpW-xKYy0a3P4ds8SdWAOCFBBb-LuZAR9GcUBa5HTcrWhkV13-j6jetnx8bLeYRzgBvrygFg").Get("https://ca-1.platform.sh/api/projects/zis3mqzwuqnu4/environments/" + environment_id)
+	resp, err := client.R().SetHeader("Accept", "application/json").SetAuthToken("eyJhbGciOiJFUzI").Get("https://ca-1.platform.sh/api/projects/zis3mqzwuqnu4/environments/" + environment_id)
 	if err != nil {
 		log.Fatal(err)
 	}
 	//defer resp.Body.Close()
-	// Explore response object
+	// Explore response object (optional)
 	fmt.Println("Response Info:")
 	fmt.Println("  Error      :", err)
 	fmt.Println("  Status Code:", resp.StatusCode())
@@ -74,6 +74,8 @@ func resourceServerRead(d *schema.ResourceData, m interface{}) error {
 	fmt.Println("  Received At:", resp.ReceivedAt())
 	fmt.Println("  Body       :\n", resp)
 	fmt.Println()
+
+      //  Map the response body to resource schema attributes
 
 	return nil
 }
