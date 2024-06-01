4ef15db55048a4ec9c790f890c2470e9560c2959
Deepak-Vohra <dvohra10@yahoo.com>
2024-06-01 14:52:51 -0700

    Update terraform-platform-plugin.go

--- terraform-platform-plugin.go
+++ terraform-platform-plugin.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: func() terraform.ResourceProvider {
+		ProviderFunc: func() *schema.Provider {
 			return Provider()
 		},
 	})
