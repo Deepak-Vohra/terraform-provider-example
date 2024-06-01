6991a00e44447b14064f44ad394d29fc5bba3034
Deepak-Vohra <dvohra10@yahoo.com>
2024-05-31 18:18:26 -0700

    Update resource_server.go

--- resource_server.go
+++ resource_server.go
@@ -6,7 +6,7 @@ import (
 	"log"
 
 	"github.com/go-resty/resty/v2"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 func resourceServer() *schema.Resource {
